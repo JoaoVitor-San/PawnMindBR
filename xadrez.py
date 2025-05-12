d0e2f1153fc788680d52766f44e79612dae3e5c2
JoaoVitor-San <joao.sant.dev@gmail.com>
2025-05-12 20:33:32 -0300

    teste do tabuleiro

--- xadrez.py
+++ xadrez.py
@@ -1,4 +1,21 @@
 import chess
 
-tabuleiro = chess.Board()
-print(tabuleiro)
+def mostrar_tabuleiro(tabuleiro):
+    print(tabuleiro)
+
+def jogar():
+    tabuleiro = chess.Board()
+    mostrar_tabuleiro(tabuleiro)
+    
+    while not tabuleiro.is_game_over():
+        movimento = input("\nDigite seu movimento (ex: e2e4): ")
+        
+        if chess.Move.from_uci(movimento) in tabuleiro.legal_moves:
+            tabuleiro.push(chess.Move.from_uci(movimento))
+            mostrar_tabuleiro(tabuleiro)
+        else:
+            print("Movimento inválido. Tente novamente.")
+    
+    print("\nJogo finalizado!")
+    
+jogar()
